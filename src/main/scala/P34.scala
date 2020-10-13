c514f8acbb22493f39ef8520a45774b420094cb9
SeanMa-Kr <seanma@uangel.com>
2020-10-13 13:03:33 +0900

    P34

--- src/main/scala/P34.scala
+++ src/main/scala/P34.scala
@@ -1,7 +1,13 @@
 package org.p99.scala
 
+import org.p99.scala.P13.encodeDirect
+
 object P34 {
 
-  // TODO
+  def totient(i: Int) = {
+    def _totient(i: Int, div: Int): List[Int] =
+      if (div > i) Nil else if (i % div == 0) div :: _totient(i / div, div) else _totient(i, div + 1)
+    if(i == 1) 1 else encodeDirect(_totient(i, 2)).map(elem => math.pow(elem._2,elem._1) - math.pow(elem._2,elem._1-1)).product.toInt
+  }
 
 }
--- src/test/scala/P34Spec.scala
+++ src/test/scala/P34Spec.scala
@@ -1,10 +1,16 @@
 package org.p99.scala
 
+import org.p99.scala.P34.totient
 import org.scalatest._
 
 class P34Spec extends UnitSpec {
 
-  // TODO
+  it should "calculate Euler's totient function phi(m)" in {
+    assert(totient(10) == 4)
+    assert(totient(28) == 12)
+    assert(totient(65) == 48)
+    assert(totient(79) == 78)
+  }
 
 }
 
